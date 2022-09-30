5e31e6dfa24f6247e90b5c9f50de44f0fb400808
Rafael Cavasani <rafael.cavasani@hotmail.com>
2022-09-29 21:44:22 -0300

    Adiciona adapter

--- src/test/kotlin/com/design/patterns/structural/adapter/AdapterTest.kt
+++ src/test/kotlin/com/design/patterns/structural/adapter/AdapterTest.kt
@@ -9,8 +9,10 @@ class AdapterTest {
         //it works because TV has HDMI port
         val computer = Computer()
         computer.connectPort(TV())
+        computer.sendImageAndSound("image", "sound")
 
         //it needs the adapter
         computer.connectPort(HDMIToVGAAdapter(OldMonitor()))
+        computer.sendImageAndSound("image", "sound")
     }
 }
